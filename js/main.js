68973755e3383c7b78418cf5878ca517549dd760
CarloColletti~ <carlocolletti5@gmail.com>
2023-02-22 18:17:10 +0100

    footer con vue

--- js/main.js
+++ js/main.js
@@ -5,7 +5,29 @@ createApp({
         return {
             linksName: [
                 'Home', 'Pages', 'Courses', 'Features', 'Blog', 'Shop',
-            ]
+            ],
+
+            addressFooter: [
+                '982 NE 191st sT # 87394 Miami, FL 33179-3899',
+                '1+(305) 5747-9909 (9am -5pn EST, Monday - Friday)',
+                'support@maxchoach.com',
+            ],
+            ExploreFooter: [
+                'Start Here',
+                'Blog',
+                'About Us',
+            ],
+            otherExploreFooter: [
+                'Success story',
+                'Couses',
+                'Contact Us',
+            ],
+            informationFooter: [
+                'Membership',
+                'Purchase Guide',
+                'Privacy policy',
+                'Terms of Services',
+            ],
         }
     },
 
